097911138ced033f669e691df1db0d0b0b17a268
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2024-05-13 19:03:22 +0530

    deploy

--- client/src/Context/Context.js
+++ client/src/Context/Context.js
@@ -6,54 +6,54 @@ import createJsonShip from "../Utilities/ShipJSON";
 export const Cart = createContext();
 
 const Context = ({ children }) => {
-  const [portData, setPortData] = useState([]);
-  const [ports, setPorts] = useState({});
-  const [ships, setShips] = useState({});
-  const [timeZone, setTimeZone] = useState([]);
-  useEffect(() => {
-    const Fetch = async () => {
-      try {
-        const { data } = await axios.get("http://localhost:4000/portdata");
+	const [portData, setPortData] = useState([]);
+	const [ports, setPorts] = useState({});
+	const [ships, setShips] = useState({});
+	const [timeZone, setTimeZone] = useState([]);
+	useEffect(() => {
+		const Fetch = async () => {
+			try {
+				const { data } = await axios.get("http://localhost:5000/portdata");
 
-        const result = await axios.get("http://localhost:4000/shipdata");
-        const shipLocation = createJsonShip(result.data.result);
+				const result = await axios.get("http://localhost:5000/shipdata");
+				const shipLocation = createJsonShip(result.data.result);
 
-        const responseTimeZone = await axios.get(
-          "http://api.timezonedb.com/v2.1/list-time-zone?key=L84TDOCF3C1Q&format=json"
-        );
+				const responseTimeZone = await axios.get(
+					"http://api.timezonedb.com/v2.1/list-time-zone?key=L84TDOCF3C1Q&format=json"
+				);
 
-        setTimeZone(responseTimeZone.data.zones);
+				setTimeZone(responseTimeZone.data.zones);
 
-        const res = createJson(data.port_geo_location);
-        setPorts(res);
-        setPortData(data.port_geo_location);
+				const res = createJson(data.port_geo_location);
+				setPorts(res);
+				setPortData(data.port_geo_location);
 
-        setShips(shipLocation);
-      } catch (error) {
-        alert("Some error occured! Please check internet and refresh");
-        console.log("Some error", error);
-      }
-    };
-    setInterval(async () => {
-      try {
-        const result = await axios.get("http://localhost:4000/shipdata");
-        const shipLocation = createJsonShip(result.data.result);
+				setShips(shipLocation);
+			} catch (error) {
+				alert("Some error occured! Please check internet and refresh");
+				console.log("Some error", error);
+			}
+		};
+		setInterval(async () => {
+			try {
+				const result = await axios.get("http://localhost:5000/shipdata");
+				const shipLocation = createJsonShip(result.data.result);
 
-        setShips(shipLocation);
-      } catch (error) {
-        alert("Some error occured! Please check internet and refresh");
-        console.log("Some error", error);
-      }
-    }, 1000 * 60);
+				setShips(shipLocation);
+			} catch (error) {
+				alert("Some error occured! Please check internet and refresh");
+				console.log("Some error", error);
+			}
+		}, 1000 * 60);
 
-    Fetch();
-  }, []);
+		Fetch();
+	}, []);
 
-  return (
-    <Cart.Provider value={{ portData, ports, ships, timeZone }}>
-      {children}
-    </Cart.Provider>
-  );
+	return (
+		<Cart.Provider value={{ portData, ports, ships, timeZone }}>
+			{children}
+		</Cart.Provider>
+	);
 };
 
 export default Context;
--- server/index.js
+++ server/index.js
@@ -4,66 +4,81 @@ const fs = express("fs");
 const exceltoJson = require("convert-excel-to-json");
 const cors = require("cors");
 const { getMinutes, modifyJsonObject } = require("./Functions/getKeyString.js");
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 app.use(cors());
 
 const result = exceltoJson({
-  sourceFile: "./port_geo_location.xlsx",
-  columnToKey: {
-    A: "portname",
-    B: "latitude",
-    C: "longitude",
-  },
+	sourceFile: "./port_geo_location.xlsx",
+	columnToKey: {
+		A: "portname",
+		B: "latitude",
+		C: "longitude",
+	},
 });
 
 const ships = exceltoJson({
-  sourceFile: "./geo_stats_data_7_days.xlsx",
-  columnToKey: {
-    A: "ship_name",
-    B: "latitude",
-    C: "longitude",
-    D: "heading",
-    E: "timestamp",
-  },
+	sourceFile: "./geo_stats_data_7_days.xlsx",
+	columnToKey: {
+		A: "ship_name",
+		B: "latitude",
+		C: "longitude",
+		D: "heading",
+		E: "timestamp",
+	},
 });
 
 const convertDateToMinutes = (value) =>
-  Math.floor(
-    (new Date(value).getTime() - new Date("2024-04-28 19:38:00.000 +0530")) /
-      (60 * 1000)
-  );
+	Math.floor(
+		(new Date(value).getTime() - new Date("2024-04-28 19:38:00.000 +0530")) /
+			(60 * 1000)
+	);
 
 const modifiedJson = modifyJsonObject(
-  ships,
-  "timestamp",
-  "minutes",
-  convertDateToMinutes
+	ships,
+	"timestamp",
+	"minutes",
+	convertDateToMinutes
 );
 
-const dat = new Date();
-console.log(getMinutes(dat));
+// -============== Deploy ======================
+let __dirname1 = path.resolve();
 
-console.log(modifiedJson);
+let pathSegments = path.dirname(__dirname1).split(path.sep);
+__dirname1 = path.join(...pathSegments);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname1, "frontend/build/index.html"));
+	});
+} else {
+	app.listen(5000, () => {
+		console.log("Server is runnig 😍😎");
+	});
+}
+
+// =============== Deploy -=====================
 
 app.get("/shipdata", (req, res) => {
-  const currentDate = new Date();
-  const minute = getMinutes(currentDate);
+	const currentDate = new Date();
+	const minute = getMinutes(currentDate);
+	const array = [];
+	console.log(minute);
+	modifiedJson.geo_stats.forEach((element) => {
+		if (element.minutes === minute) {
+			console.log(element);
+			array.push(element);
+		}
+	});
 
-  const array = [];
-  console.log(minute);
-  modifiedJson.geo_stats.forEach((element) => {
-    if (element.minutes === minute) {
-      console.log(element);
-      array.push(element);
-    }
-  });
-
-  res.status(200).json({ result: array });
+	res.status(200).json({ result: array });
 });
 
 app.get("/portdata", (req, res) => {
-  res.status(200).json(result);
-});
-
-app.listen(4000, () => {
-  console.log("Server is runnig 😍😎");
+	res.status(200).json(result);
 });
