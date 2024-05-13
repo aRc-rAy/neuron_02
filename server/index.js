bc9325a82b5446422eb1508471de3b4412990a35
Dilip Kumar Yadav <codingiitiandilip@gmail.com>
2024-05-13 19:35:50 +0530

    final deploy

--- server/index.js
+++ server/index.js
@@ -20,29 +20,29 @@ const result = exceltoJson({
 	},
 });
 
-const ships = exceltoJson({
-	sourceFile: "./geo_stats_data_7_days.xlsx",
-	columnToKey: {
-		A: "ship_name",
-		B: "latitude",
-		C: "longitude",
-		D: "heading",
-		E: "timestamp",
-	},
-});
+// const ships = exceltoJson({
+// 	sourceFile: "./geo_stats_data_7_days.xlsx",
+// 	columnToKey: {
+// 		A: "ship_name",
+// 		B: "latitude",
+// 		C: "longitude",
+// 		D: "heading",
+// 		E: "timestamp",
+// 	},
+// });
 
-const convertDateToMinutes = (value) =>
-	Math.floor(
-		(new Date(value).getTime() - new Date("2024-04-28 19:38:00.000 +0530")) /
-			(60 * 1000)
-	);
+// const convertDateToMinutes = (value) =>
+// 	Math.floor(
+// 		(new Date(value).getTime() - new Date("2024-04-28 19:38:00.000 +0530")) /
+// 			(60 * 1000)
+// 	);
 
-const modifiedJson = modifyJsonObject(
-	ships,
-	"timestamp",
-	"minutes",
-	convertDateToMinutes
-);
+// const modifiedJson = modifyJsonObject(
+// 	ships,
+// 	"timestamp",
+// 	"minutes",
+// 	convertDateToMinutes
+// );
 
 // -============== Deploy ======================
 let __dirname1 = path.resolve();
